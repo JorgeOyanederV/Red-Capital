4199b802a8fd0e74d06ab9aee1f3eb4cfae31995
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2022-01-05 12:27:56 -0300

    adjust size of the screen

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,35 @@
 <template>
-  <Navbar/>
-  <router-view></router-view>
+  <div class="app">
+    <div class="navbar">
+      <Navbar />
+    </div>
+
+    <div class="body">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
-import  Navbar  from './shared/Navbar'
-import M from 'materialize-css'
+import Navbar from '@/shared/Navbar'
 
 export default {
+  data(){
+    return {
+      }
+    
+  },
   components:{
     Navbar
   },
-  mounted () {
-    M.AutoInit()
-},
+  
 }
 </script>
 
-<style>
-
+<style scoped>
+.app{
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
 </style>
--- frontend/src/shared/Navbar.vue
+++ frontend/src/shared/Navbar.vue
@@ -1,27 +1,37 @@
 <template>
-   <nav>
     <div class="nav-wrapper">
-      <img src="https://www.khipu.com/wp-content/uploads/2021/09/Red-Capital.png" alt="image" class="vue-logo">
-      <ul id="nav-mobile" class="right hide-on-small-only">
-        <li><router-link to='/home'>INICIO</router-link></li>
-        <li><router-link to='/about'>QUIENES SOMOS</router-link></li>
-        <li><router-link to='/post'>PROGRAMAS</router-link></li>
-        <li><router-link to='/how'>COMO FUNCIONA</router-link></li>
-      </ul>
+      <img src="https://www.khipu.com/wp-content/uploads/2021/09/Red-Capital.png" alt="image" >
+      <div class="links">
+        <router-link to='/home'>INICIO</router-link>
+        <router-link to='/about'>QUIENES SOMOS</router-link>
+        <router-link to='/post'>PROGRAMAS</router-link>
+        <router-link to='/how'>COMO FUNCIONA</router-link>
+      </div>
     </div>
-  </nav>
 </template>
 
 <script>
-export default {
 
+export default {
+  data(){
+    return {
+      
+    }
+  }
 }
 </script>
 
 <style scoped>
-.vue-logo{
-   float:left;
-   height: 100%;
-   margin-left: 5px;
+.nav-wrapper{
+  display: flex;
+  justify-content: space-between;
+  height: 10vh;
+  padding: 0.5rem 2rem;
+}
+.links{
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+  gap: 2rem;
 }
 </style>
