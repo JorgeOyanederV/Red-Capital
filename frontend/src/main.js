72da351b1c35262fc96b41d4bde6b07e72dde083
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2022-01-05 12:26:34 -0300

    disable materialize, add normalize and global css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'materialize-css/dist/css/materialize.min.css'
-import 'material-design-icons/iconfont/material-icons.css'
+import 'normalize.css'
+import '@/shared/global.css'
 
 createApp(App)
    .use(store)
